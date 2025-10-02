2689ac68b6e296abb79edbd7e2aa95d9a523f457
Matheus Sousa <dev.matheus.contato@gmail.com>
2025-10-02 08:48:48 -0300

    testando commit pela idea

--- src/main/java/com/PayManger/pagamentos/App/Main.java
+++ src/main/java/com/PayManger/pagamentos/App/Main.java
@@ -4,17 +4,55 @@ import com.PayManger.pagamentos.model.Cliente;
 import com.PayManger.pagamentos.model.Pedido;
 import com.PayManger.pagamentos.model.Produto;
 import com.PayManger.pagamentos.model.FormasDePagamento;
+import com.PayManger.pagamentos.service.ClienteService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+    Cliente cliente1;
+    public void menuInterativo(){
+        int iniciarSistema = 1;
+        do {
+
+
+            System.out.println("----Loja de Camisetas-----");
+
+            System.out.println("'1 -> Cadastrar cliente!");
+            System.out.println("'2 -> Listar cliente!");
+            System.out.println("'3 -> Cadastrar Produto!");
+            System.out.println("'4 -> Listar Produto!");
+            System.out.println("'5 -> Finalizar Pagamento!");
+            System.out.println("'0 -> Para sair!");
+
+            System.out.print("Escolha uma opcao: ");
+
+            int opcao = scanner.nextInt();
+            switch (opcao){
+                case 1:
+                    ClienteService clienteService = new ClienteService();
+                    clienteService.cadastrarCliente(cliente1);
+
+
+
+            }
+        }while(iniciarSistema != 0);
+
+    }
     public static void main(String[] args) {
 
 
 //        FormasDePagamento pagamento = new FormasDePagamento();
 //        pagamento.pagamentoBoleto("1231241234123123");
 
+
+
+
+        Main test = new Main();
+        test.menuInterativo();
 
 
 
